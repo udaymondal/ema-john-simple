7d57fe84594d1da3666593a7b70eae6a405cf126
udaymondal <udaysk.mondal@gmail.com>
2021-02-28 20:11:39 +0600

    done

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -18,13 +18,20 @@ const Cart = (props) => {
     else if (total > 0){
         shipping = 0;
     }
+    
+    const roundedTotal = total.toFixed(2);
+    const tax = (total/10).toFixed(2);
+   
+    const grandTotal = (Number(total) + shipping + Number(tax)).toFixed(2);
+    
     return (
         <div>
             <h4>Order Summary</h4>
             <p>Items Ordered: {cart.length}</p>
-            <p>Product Price: {total}</p>
+            <p>Product Price: {roundedTotal}</p>
             <p><small>Shipping Cost: {shipping}</small></p>
-            <p>Total Price: {total + shipping}</p>
+            <p><small>Tax/VAT: {tax}</small></p>
+            <p><b>Total Price: {grandTotal}</b></p>
         </div>
     );
 };
